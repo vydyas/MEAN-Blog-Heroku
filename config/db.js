71aa431b2599d3a300f983479592b09032cc6c09
Siddhu <vydyas@gmail.com>
2017-05-28 15:53:32 +0530

    db url changed

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports={
-    localURL:'mongodb://vydyas:siddhu123@ds011419.mlab.com:11419/vydyas'
+    //localURL:'mongodb://vydyas:siddhu123@ds011419.mlab.com:11419/vydyas'
+    localURL:'mongodb://heroku_80hk6526:q04rci3jsinef55lik2amjhlcs@ds155811.mlab.com:55811/heroku_80hk6526'
 }
