939517017025b273d543df65982b16cea0d6b95c
Siddhu <vydyas@gmail.com>
2017-05-29 06:56:05 +0530

    Added proces.env.mongourl

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports={
-    localURL:'mongodb://heroku_zqblfh0z:lifl7r7leo55i1phk26paacvg@ds155811.mlab.com:55811/heroku_zqblfh0z'
+    localURL: process.env.MONGOLAB_URI || 'mongodb://heroku_zqblfh0z:lifl7r7leo55i1phk26paacvg@ds155811.mlab.com:55811/heroku_zqblfh0z'
 }
